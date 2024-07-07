efa641c351888e29d0551260ae0a0fe22e169c8b
Andrey Fedyukin <andreynf@mail.ru>
2024-07-07 03:41:04 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// Цвет хедера при скроле ***************************
+let header = document.querySelector(".header");
+window.onscroll = function () {
+  if (window.scrollY > 100) {
+    header.style.background = "rgba(0, 0, 0, 0.5)";
+  } else {
+    header.style.background = "rgb(9, 15, 29)";
+  }
+};
+
+// Бургер ***************************
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger").addEventListener("click", function () {
+    document.querySelector(".header").classList.toggle("open");
+  });
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    document.querySelector(".header").classList.remove("open");
+  }
+});
+
+document.getElementById("menu").addEventListener("click", (event) => {
+  event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener("click", (event) => {
+  event._isClickWithInMenu = true;
+});
+document.body.addEventListener("click", (event) => {
+  if (event._isClickWithInMenu) return;
+  document.querySelector(".header").classList.remove("open");
+});
+
+// Модальное окно ***************************
+document
+  .getElementById("open-modal-btn")
+  .addEventListener("click", function () {
+    document.getElementById("my-modal").classList.add("open");
+  });
+
+document
+  .getElementById("close-my-modal-btn")
+  .addEventListener("click", function () {
+    document.getElementById("my-modal").classList.remove("open");
+  });
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    document.getElementById("my-modal").classList.remove("open");
+  }
+});
+
+document
+  .querySelector("#my-modal .modal__box")
+  .addEventListener("click", (event) => {
+    event._isClickWithInModal = true;
+  });
+document.getElementById("my-modal").addEventListener("click", (event) => {
+  if (event._isClickWithInModal) return;
+  event.currentTarget.classList.remove("open");
+});
