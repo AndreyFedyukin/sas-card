512a36ccd420c7063fbeb9aad252092e82649220
Andrey Fedyukin <andreynf@mail.ru>
2024-07-13 07:25:57 +0300

    переписал js модалки под классы

--- js/main.js
+++ js/main.js
@@ -34,29 +34,29 @@ document.body.addEventListener("click", (event) => {
 
 // Модальное окно ***************************
 document
-  .getElementById("open-modal-btn")
+  .querySelector(".open-modal-btn")
   .addEventListener("click", function () {
-    document.getElementById("my-modal").classList.add("open");
+    document.querySelector(".my-modal").classList.add("open");
   });
 
 document
-  .getElementById("close-my-modal-btn")
+  .querySelector(".close-my-modal-btn")
   .addEventListener("click", function () {
-    document.getElementById("my-modal").classList.remove("open");
+    document.querySelector(".my-modal").classList.remove("open");
   });
 
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    document.getElementById("my-modal").classList.remove("open");
+    document.querySelector(".my-modal").classList.remove("open");
   }
 });
 
 document
-  .querySelector("#my-modal .modal__box")
+  .querySelector(".my-modal .modal__box")
   .addEventListener("click", (event) => {
     event._isClickWithInModal = true;
   });
-document.getElementById("my-modal").addEventListener("click", (event) => {
+document.querySelector(".my-modal").addEventListener("click", (event) => {
   if (event._isClickWithInModal) return;
   event.currentTarget.classList.remove("open");
 });
